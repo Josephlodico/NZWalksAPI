3a9c554182e22deb4236e4af4cdeb78b42a2855a
Joseph Lodico <josephl@addatech.ca>
2026-06-15 11:39:44 -0400

    Created Walks controller and repository implementation

---
+++ NZWalks.API/Models/DTO/WalkDto.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class WalkDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Descriptions { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageURl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+    }
+}
