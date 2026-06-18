1ff738e2114c4ee9efe134a327fcd4bd9e397b4f
Joseph Lodico <josephl@addatech.ca>
2026-06-18 16:59:45 -0400

    - Added Authorization To Swagger.

--- NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2ad76a6f04b4062fedc281a0413ba841ba4430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556fb9f595e870626a5ff32604115c67cf53ae43")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
