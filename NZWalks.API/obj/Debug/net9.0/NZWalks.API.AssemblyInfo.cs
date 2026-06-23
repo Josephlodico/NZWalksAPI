e91b6f64bf1122204fba328771a0f6e65b8730da
Joseph Lodico <josephl@addatech.ca>
2026-06-23 16:49:28 -0400

    - DELETE - Deleting a Resource using Web API. - Section 12 Finished.

--- NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b493f3e758b1d33a0161e0148021e9cf21999d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1f66925c92e06a58019b1ff4f0861a9eb83279")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -105,5 +105,26 @@ namespace NZWalks.UI.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7092/api/regions/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+                // Console
+            }
+
+            return View("Edit");
+        }
     }
 }
